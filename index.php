a4723a58574e1481b8aab7f179ff3f8e9473e80f
Welldone MBOYI <mboyibienfait@gmail.com>
2022-11-08 12:23:48 +0100

    La premiere version de mon projet des stages 2A AP2

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// require "views/page.login.template2.view.php";
+//require "views/dashboard.admin.template.view.php";
+// include "views/page.login.template2.view.php";
+// include "views/dashboard.user.template.view.php";
+// include "views/page.login.template.view.php";
+
+require_once "vendor/autoload.php";
+
+use HelloWorld\SayHello;
+
+echo SayHello::world();
---
+++ views/acceuil.view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p><a href="/views/page.login.template2.view.php">connexion</a></p>
+    <p><a href="/views/dashboard.admin.template.view.php">Tableau de bord Amin</a></p>
+    <p><a href="/views/dashboard.user.template.view.php">Tableau de bord User</a></p>
+</body>
+
+</html>
